68e65a30e649487df3eca786eaa85d1e4b390bd8
brijrajsinh parmar <brijrajparmar27@gmail.com>
2023-02-06 09:21:16 +0530

    fixes

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,48 +1,64 @@
 import { useState } from "react";
 import { io } from "socket.io-client";
 import "./Home.css";
+
+const socket = io("http://localhost:5000/");
+
 const Home = () => {
-  const [msg, setMsg] = useState([]);
-  const sendMessage = (newText) => {
-    socket.emit("send", newText);
-    setMsg((prev) => [...prev, newText]);
-  };
-  const handleMsg = (e) => {
+
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const [room,setRoom] = useState("");
+
+  const sendChat = (e) => {
     e.preventDefault();
-    sendMessage(e.target.msg.value.trim());
-    e.target.reset();
+    socket.emit("chat-send", message);
+    setMessage("");
   };
-  const handleRoom = (e) => {
+
+  const changeRoom = (e) =>{
     e.preventDefault();
-    console.log(e.target.room.value.trim());
-    e.target.reset();
-  };
-  const socket = io("http://localhost:3000/");
-  socket.on("connect", () => {
-    sendMessage("connected");
+    room && socket.emit("change-room", room);
+    console.log("room changed")
+  }
+
+  socket.on("chat-recieve", (payload) => {
+    setChat([...chat, payload]);
   });
+
   return (
     <div className="home">
       <div className="chats">
-        <div className="chats_contain"></div>
+        <div className="chats_contain">
+          {
+            chat && chat.map((each, index) => {
+              return <p key={index}>{each}</p>
+            })
+          }
+        </div>
       </div>
       <div className="inputs">
         <div className="input_contain">
-          <form className="msg_form" onSubmit={handleMsg}>
+          <form className="msg_form" onSubmit={sendChat}>
             <input
               type="text"
               className="msg input_tb"
               name="msg"
               placeholder="write message"
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
             />
             <input type="submit" value="send" className="submit" />
           </form>
-          <form className="room_form" onSubmit={handleRoom}>
+          {/* <form className="room_form" onSubmit={handleRoom}> */}
+          <form className="room_form" onSubmit={changeRoom}>
             <input
               type="text"
               className="room  input_tb"
               name="room"
               placeholder="enter room id"
+              onChange={(e)=>{setRoom(e.target.value)}}
             />
             <input type="submit" value="connect" className="submit" />
           </form>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,34 @@
-const io = require("socket.io")(3000, {
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = app.listen(5000);
+
+const io = socketio(server, {
   cors: {
-    origin: "*",
-  },
+    origin: '*',
+  }
 });
 
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  socket.on("send", (message) => {
-    console.log(message);
-  });
-});
+
+  console.log("connected to ", socket.id);
+
+  io.on("disconnect", () => {
+    console.log("disconnected to ", socket.id)
+  })
+
+  socket.on("chat-send", (paload) => {
+    console.log(socket.id, " says ", paload)
+    io.emit("chat-recieve", paload)
+  })
+
+  socket.on("change-room", payload=>{
+    socket.join(payload);
+    console.log("room switched to ",payload)
+  })
+
+})
